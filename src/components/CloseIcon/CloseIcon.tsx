a8916a139e68160943ccf702aa4958758e8db81a
r-liciotti <r.liciotti@gmail.com>
2024-09-26 11:19:43 +0200

    update

--- src/components/CloseIcon/CloseIcon.tsx
+++ src/components/CloseIcon/CloseIcon.tsx
@@ -7,26 +7,26 @@ function CloseIcon({ handleOnClick }: CloseIconProps) {
   return (
     <div className={style.closeIconContainer} onClick={handleOnClick}>
       <svg
-        width="15"
-        height="15"
-        className={style.closeIcon}
+        width="20"
+        height="20"
         viewBox="0 0 15 15"
+        className={style.closeIcon}
       >
-        <g id="Canvas" fill="none">
-          <g id="close">
+        <g fill="none">
+          <g>
             <path
               d="M2 2l11 11"
               stroke-width="2"
+              stroke-miterlimit="10"
               stroke-linecap="square"
-              stroke="#111"
-              id="Vector"
+              stroke="#121212"
             ></path>
             <path
               d="M13 2L2 13"
               stroke-width="2"
+              stroke-miterlimit="10"
               stroke-linecap="square"
-              stroke="#111"
-              id="Vector_2"
+              stroke="#121212"
             ></path>
           </g>
         </g>
